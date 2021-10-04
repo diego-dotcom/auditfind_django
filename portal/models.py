76711652c5042fe3695065b67f93631f41c9320d
diego-dotcom <mendizabal.diego@gmail.com>
2021-10-04 20:11:34 -0300

    Se agregan funcionalidades para el home

--- portal/models.py
+++ portal/models.py
@@ -3,8 +3,15 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Seccion(models.Model):
+    descripcion = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.descripcion
+
 class Articulo(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE, related_name="seccion")
     titulo = models.CharField(max_length=100)
     copete = models.TextField()
     texto = models.TextField()
@@ -17,6 +24,7 @@ class Articulo(models.Model):
 
     def __str__(self):
         return self.titulo
+
 
 
 
--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Articulo
 
 # Create your views here.
+
+def home(request):
+    articulos_home = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')[:3]
+    return render(request, "portal/index.html", {
+        'articulos': articulos_home
+    })
